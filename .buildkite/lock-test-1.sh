b32dc559acfcb36248c76ff8be480cdb0c93950a
Amna Akram <amna@buildkite.com>
2025-02-27 12:48:34 -0800

    Update lock-test-1.sh

--- .buildkite/lock-test-1.sh
+++ .buildkite/lock-test-1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Step 1: Acquiring lock..."
-buildkite-agent lock acquire my-lock
+LOCK_TOKEN=$(buildkite-agent lock acquire my-lock)
 
 echo "Step 1: Lock acquired. Sleeping for 60 seconds..."
 sleep 60
 
 echo "Step 1: Releasing lock..."
-buildkite-agent lock release my-lock
+buildkite-agent lock release my-lock "$LOCK_TOKEN"
